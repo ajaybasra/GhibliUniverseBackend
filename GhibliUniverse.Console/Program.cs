a3c8782ed9bf51d570db908a7510cd3f201a950c
Ajay Basra <ajay_b@hotmail.co.nz>
2023-06-19 15:25:23 +1200

    refactor: program.cs file has dependencies wired up using service provider, there is much less newing of objects now

--- GhibliUniverse.Console/Program.cs
+++ GhibliUniverse.Console/Program.cs
@@ -15,15 +15,25 @@ services.AddDbContext<GhibliUniverseContext>(options =>
 {
     options.UseNpgsql(Configuration.GetDbConnectionString());
 });
+services.AddScoped<IFilmRepository, FilmRepository>();
+services.AddScoped<IVoiceActorRepository, VoiceActorRepository>();
+services.AddScoped<IReviewRepository, ReviewRepository>();
+services.AddScoped<IFilmService, FilmService>();
+services.AddScoped<IVoiceActorService, VoiceActorService>();
+services.AddScoped<IReviewService, ReviewService>();
+
 var serviceProvider = services.BuildServiceProvider();
 using var scope = serviceProvider.CreateScope();
-var context = scope.ServiceProvider.GetRequiredService<GhibliUniverseContext>();
-var filmRepository = new FilmRepository(context);
-var voiceActorRepository = new VoiceActorRepository(context);
-var reviewRepository = new ReviewRepository(context);
-var filmService = new FilmService(filmRepository);
-var voiceActorService = new VoiceActorService(voiceActorRepository);
-var reviewService = new ReviewService(reviewRepository);
+
+var filmService = scope.ServiceProvider.GetRequiredService<IFilmService>();
+var voiceActorService = scope.ServiceProvider.GetRequiredService<IVoiceActorService>();
+var reviewService = scope.ServiceProvider.GetRequiredService<IReviewService>();
+
+var commandLine = new CommandLine();
+var consoleWriter = new ConsoleWriter();
+var argumentProcessor = new ArgumentProcessor(commandLine, consoleWriter, filmService, reviewService, voiceActorService);
+argumentProcessor.Process();
+// reviewService.DeleteReview(Guid.Parse("1fbc6079-9f7d-417f-92a4-843d007edcb4"));
 // review.CreateReview(Guid.Parse("ad25f890-3050-46f4-951f-786c563c8ff9"), 10);
 // voice.CreateVoiceActor("John Cena");
 // film.LinkVoiceActor(Guid.Parse("0af40df1-a634-445b-be1f-0b5e671d1c16"), Guid.Parse("6408242e-8358-40c2-b3ad-5cb66dc68f89"));
@@ -34,20 +44,11 @@ var reviewService = new ReviewService(reviewRepository);
 // voice.DeleteVoiceActor(Guid.Parse("151148f4-48fc-4729-86fa-8c3071cfa02d"));
 Console.WriteLine(filmService.GetAllFilms()[0]);
 
-var commandLine = new CommandLine();
-var consoleWriter = new ConsoleWriter();
-var fileOperations = new FileOperations();
-var filmPersistence = new FilmPersistence(fileOperations);
-var reviewPersistence = new ReviewPersistence(fileOperations);
-var voiceActorPersistence = new VoiceActorPersistence(fileOperations);
-var filmVoiceActorPersistence = new FilmVoiceActorPersistence(fileOperations, filmPersistence, voiceActorPersistence);
-// var filmService = new FilmService(filmPersistence, reviewPersistence, voiceActorPersistence, filmVoiceActorPersistence);
-var argumentProcessor = new ArgumentProcessor(commandLine, consoleWriter, filmService, reviewService, voiceActorService);
-argumentProcessor.Process();
-var g = filmService.BuildFilmList();
-Console.WriteLine(g);
-var y = voiceActorService.BuildVoiceActorList();
-// Console.WriteLine(y);
-var x = reviewService.BuildReviewList();
-// Console.WriteLine(x);
-var root = Directory.GetCurrentDirectory();
+
+// var g = filmService.BuildFilmList();
+// Console.WriteLine(g);
+// var y = voiceActorService.BuildVoiceActorList();
+// // Console.WriteLine(y);
+// var x = reviewService.BuildReviewList();
+// // Console.WriteLine(x);
+// var root = Directory.GetCurrentDirectory();
