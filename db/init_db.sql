ca7d4bc52929867e7cef662a5b2981be63bcc4cc
Ajay Basra <ajay_b@hotmail.co.nz>
2023-08-21 10:23:02 +1200

    feat: updated _init.db.sql

--- db/init_db.sql
+++ db/init_db.sql
@@ -45,16 +45,50 @@ CREATE INDEX "IX_Reviews_FilmId" ON "Reviews" ("FilmId");
 INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
 VALUES ('20230615213710_InitialCreate', '7.0.5');
 
-INSERT INTO "Films" ("Id", "Title_Value", "Description_Value", "Director_Value", "Composer_Value", "ReleaseYear_Value")
+COMMIT;
+
+START TRANSACTION;
+
+ALTER TABLE "VoiceActors" DROP COLUMN "Name_Value";
+
+ALTER TABLE "Films" DROP COLUMN "Composer_Value";
+
+ALTER TABLE "Films" DROP COLUMN "Description_Value";
+
+ALTER TABLE "Films" DROP COLUMN "Director_Value";
+
+ALTER TABLE "Films" DROP COLUMN "Title_Value";
+
+ALTER TABLE "Reviews" RENAME COLUMN "Rating_Value" TO "Rating";
+
+ALTER TABLE "Films" RENAME COLUMN "ReleaseYear_Value" TO "ReleaseYear";
+
+ALTER TABLE "VoiceActors" ADD "Name" text NOT NULL DEFAULT '';
+
+ALTER TABLE "Films" ADD "Composer" text NOT NULL DEFAULT '';
+
+ALTER TABLE "Films" ADD "Description" text NOT NULL DEFAULT '';
+
+ALTER TABLE "Films" ADD "Director" text NOT NULL DEFAULT '';
+
+ALTER TABLE "Films" ADD "Title" text NOT NULL DEFAULT '';
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20230815025826_UpdateToNewDataModels', '7.0.5');
+
+-- INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+-- VALUES ('20230615213710_InitialCreate', '7.0.5');
+
+INSERT INTO "Films" ("Id", "Title", "Description", "Director", "Composer", "ReleaseYear")
 VALUES ('00000000-0000-0000-0000-000000000001', 'Spirited Away', 'A young girl, Chihiro, finds herself trapped in a spirit world after her parents are transformed into pigs by a witch. In order to save her parents and find a way back home, she must work in a bathhouse for spirits and navigate through various challenges and encounters.', 'Hayao Miyazaki', 'Joe Hisaishi', 2001);
 
-INSERT INTO "VoiceActors" ("Id", "Name_Value")
+INSERT INTO "VoiceActors" ("Id", "Name")
 VALUES ('00000000-0000-0000-0000-000000000001', 'Rumi Hiiragi'),
        ('00000000-0000-0000-0000-000000000002', 'Miyu Irino'),
        ('00000000-0000-0000-0000-000000000003', 'Mari Natsuki'),
        ('00000000-0000-0000-0000-000000000004', 'Bunta Sugawara');
 
-INSERT INTO "Reviews" ("Id", "Rating_Value", "FilmId")
+INSERT INTO "Reviews" ("Id", "Rating", "FilmId")
 VALUES ('00000000-0000-0000-0000-000000000001', 9, '00000000-0000-0000-0000-000000000001'),
        ('00000000-0000-0000-0000-000000000002', 10, '00000000-0000-0000-0000-000000000001');
        
